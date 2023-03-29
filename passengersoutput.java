170f1a1937fd740bef59b9843a3663aeccb17b62
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-29 15:54:18 +0530

    Add files via upload

---
+++ passengersoutput.java
@@ -0,0 +1,10 @@
+
+PS C:\Users\school\Desktop\javafiles>  & 'C:\Program Files\Java\jdk1.8.0_151\bin\java.exe' '-agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=localhost:51487' '-cp' 'C:\Users\school\AppData\Roaming\Code\User\workspaceStorage\a03b2cf5f1f8cb12ae8665fb67a59eab\redhat.java\jdt_ws\javafiles_bdc28c66\bin' 'Passengers'
+Age group       Count
+0-10            1
+10-20           2
+20-30           2
+30-40           2
+40-50           2
+50+             2
+PS C:\Users\school\Desktop\javafiles>
